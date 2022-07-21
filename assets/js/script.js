fb8f23fd2dfe6e44f49cdb445b86c3ddcb05bd11
Kasey <kasey_crocker@yahoo.com>
2022-07-21 14:56:09 -0400

    fetch both APIs

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//when I search a city, a list of listing are displayed
+
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
